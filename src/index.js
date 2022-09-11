11d89ece05b286bdb5967c26bfd7ab52acc53c01
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-09-10 21:38:59 -0500

    Setup routing to login and dashboard

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Routes
+import DashboardRoute from './routes/dashboard-route';
+import LoginAndRegistrationRoute from './routes/login-and-registration-route';
+
+// Components
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginAndRegistrationRoute/>} />
+      <Route path='/dashboard' element={<DashboardRoute/>} />
+
+      {/* 404 Route */}
+      <Route path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+      />
+    </Routes>
+  </BrowserRouter>
 );
---
+++ src/routes/dashboard-route.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, 
+         Heading, 
+         Text 
+       } from '@chakra-ui/react';
+
+const DashboardRoute = () => {
+  return (
+    <Box>
+      <Heading>Dashbaord UI Route</Heading>
+      <Text>You've made it to the dashboard using React Router!</Text>
+    </Box>
+  );
+}
+
+export default DashboardRoute;
