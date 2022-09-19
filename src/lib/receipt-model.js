fb18961cde4748c285343fd0f072a18df58c453c
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-09-19 00:05:20 -0500

    Scaffold ExpenseItem and ReceiptItem

---
+++ src/lib/receipt-model.js
@@ -0,0 +1,27 @@
+export class ReceiptItem {
+  constructor() {
+    this._itemId =
+    this._itemName = '';       
+    this._itemUnitPrice = 0.0;
+    this._itemQuantity = 1;
+    this._itemTotalPrice = 0.0; 
+  }
+  
+  /*
+    -- Getters --
+    getItemId() : number
+    getItemName() : string
+    getItemUnitPrice() : number
+    getItemQty() : number
+    getItemTotalPrice() : number
+
+    -- Setters --
+    setItemId(id: number) : void
+    setItemName(itemName: string) : void
+    setItemUnitPrice(itemUnitPrice: number) : void
+    setItemQuantity(itemQuantity: number) : void
+
+    -- Utility -- 
+    _calcualteTotalPrice() : number
+  */
+};
