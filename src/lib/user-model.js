58821994f51068516d5a2a1bc41a809f492e39d7
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-09-18 23:31:53 -0500

    Scaffold Backend Data Models
    Created the following class scaffolds: User, Expense, ExpenseItem, ExpenseCollection, Receipt, ReceiptItem, ReceiptCollection, FinancialInfo, CategoryCollection

---
+++ src/lib/user-model.js
@@ -0,0 +1,42 @@
+import { FinancialInfo } from './financial-info-model';
+import { ReceiptCollection } from './receipt-collection-model';
+import { ExpenseCollection } from './expense-collection-model';
+import { CategoryCollection } from './category-collection-model';
+
+class User {
+  constructor(firstName, lastName, id) {
+    this._id = id;                  // requried
+    this._email = '';               // required
+    this._password = '';            // required
+    this._firstName = firstName;    // required
+    this._lastName = lastName;      // required 
+    this._financialInfo = null;     // required
+    this._receiptCollection = null;
+    this._expenseCollection = null;
+    this._categoryCollection = null;
+  }
+
+  /*
+    -- Getters -- 
+    getId() : number
+    getEmail() : string
+    getPassword() : string
+    getFirstName() : string
+    getLastName() : string
+    getFinancialInfo : FinancialInfo
+    getReceiptCollection : ReceiptCollection
+    getExpenseCollection: ExpenseCollection
+    getCategoryCollection : CategoryCollection
+
+    -- Setters -- 
+    setEmail(email: string) : void
+    setPassword(password: string) : void
+    setFirstName(name: string) : void
+    setLastName(name: string) : void
+
+    -- Utilities --
+    toString() : void // maybe?
+  */
+
+
+}
