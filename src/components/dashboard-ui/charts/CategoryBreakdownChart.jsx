803026d05fb0cb6590f0f3f279b5fb335cdb9159
Chris Adamson <chrisallenadamsoniii@gmail.com>
2022-10-05 22:43:15 -0500

    Removes responsive container

--- src/components/dashboard-ui/charts/CategoryBreakdownChart.jsx
+++ src/components/dashboard-ui/charts/CategoryBreakdownChart.jsx
@@ -48,7 +48,7 @@ const data = [
 
 const CategoryBreakdownChart = () => {
   return (
-    <ResponsiveContainer width="100%" height="100%">
+   
       <LineChart
         width={500}
         height={300}
@@ -68,7 +68,7 @@ const CategoryBreakdownChart = () => {
         <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
       </LineChart>
-    </ResponsiveContainer>
+    
   );
 }
 
