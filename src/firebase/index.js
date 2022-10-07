1db68c2b2155511cf9052d180fb707c58fb7431f
DylanCommean01 <DylanCommean01@yahoo.com>
2022-10-07 10:54:36 -0500

    Initializing Firebase. Adding code to access Firestore

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./key/firebaseauth.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+export const db = getFirestore();
