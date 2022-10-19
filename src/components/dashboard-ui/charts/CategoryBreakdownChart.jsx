dbdbb865f3c54a9e1225d0bd3834c75c3f2e1811
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-19 09:35:42 -0500

    Refactor CategoryBreakdownChart
    Removed unused Chakra component imports and fix alignment in source file.

--- src/components/dashboard-ui/charts/CategoryBreakdownChart.jsx
+++ src/components/dashboard-ui/charts/CategoryBreakdownChart.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,ResponsiveContainer} from 'recharts';
-import { Box,
-  Flex,
-  Heading,
-  Badge,
-  Center,
-  Stat,
-  StatNumber,
-  Button,
-} from '@chakra-ui/react';
+
+import { Radar, 
+         RadarChart, 
+         PolarGrid, 
+         PolarAngleAxis, 
+         PolarRadiusAxis,
+         ResponsiveContainer
+       } from 'recharts';
+
+import { Flex,
+         Heading,
+         Badge,
+         Center,
+       } from '@chakra-ui/react';
 
 import UpdateModalButton from '../../modals/UpdateModalButton';
-
 
 const data = [
   {
     category: 'Food',
     A: 75,
-   
     fullMark: 120,
   },
   {
     category: 'Entertainment',
     A: 60,
-
     fullMark: 120,
   },
   {
     category: 'Utility',
     A: 66,
-    
     fullMark: 120,
   },
   {
     category: 'Transportation',
     A: 34,
-
     fullMark: 120,
   },
   {
@@ -50,56 +49,50 @@ const data = [
   }
 ];
 
-
 const CategoryBreakdownChart = () => {
   return (
-<Flex direction='column' justify='start'>
+    <Flex direction='column' justify='start'>
       <Flex justify='space-between' align='center' px='8px'>
         <Heading as='h2' fontSize='2xl'>Category Breakdown Chart</Heading>
         <Badge fontSize='xl' 
-               colorScheme='purple' 
-               py='2px' 
-               px='16px' 
-               rounded='sm'
-               variant='subtle'
+                colorScheme='purple' 
+                py='2px' 
+                px='16px' 
+                rounded='sm'
+                variant='subtle'
           >
             <Center>Weekly</Center>
           </Badge>
       </Flex>
 
       <ResponsiveContainer width='100%' height={400}>
+        <RadarChart
+          margin={{
+            top: 30,
+            right: 50,
+            left: 20,
+            bottom: 5,
+          }}
+          data={data}
+        >
+          <PolarGrid />
+          <PolarAngleAxis dataKey="category" />
+          <PolarRadiusAxis />
+          <Radar
+            wordSpacing= '20px'
+            name="user"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.7}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
 
-      <RadarChart
-    margin={{
-      top: 30,
-      right: 50,
-      left: 20,
-      bottom: 5,
-    }}
-   
-    
-    data={data}
-  >
-    <PolarGrid />
-    <PolarAngleAxis dataKey="category" />
-    <PolarRadiusAxis />
-    <Radar
-      wordSpacing= '20px'
-      name="user"
-      dataKey="A"
-      stroke="#8884d8"
-      fill="#8884d8"
-      fillOpacity={0.7}
-    />
-  </RadarChart>
-  </ResponsiveContainer>
-{/*I will update this later, this button will probably have it's own modal since it's function will be
-to add categories */}
-  <Flex justify='center' align='center'>
+      <Flex justify='center' align='center'>
         <UpdateModalButton colorScheme='purple' variant='outline' text='Add Category' width='full'/>
       </Flex>
-      </Flex>
-    
+    </Flex>
   );
 }
 
