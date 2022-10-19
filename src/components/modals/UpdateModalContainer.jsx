3c2814a7ff2df43e628c9fd623e5ec63970c32af
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-19 11:09:16 -0500

    Refactor UpdateModalButton
    Renamed to UpdateModalContainer and added props to make the component more reusable - primarily as containers for various modal types

---
+++ src/components/modals/UpdateModalContainer.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import  { Modal,
+          ModalOverlay,
+          ModalCloseButton,
+          ModalContent,
+          ModalHeader,
+          ModalBody,
+          ModalFooter,
+          Button,
+          useDisclosure,
+        } from '@chakra-ui/react';
+
+const UpdateModalContainer = ({colorScheme, btnVariant, text, width, modalTitle, children}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button colorScheme={colorScheme} 
+              variant={btnVariant} 
+              onClick={onOpen}
+              width={width}>
+        {text}
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontSize='2xl'>{modalTitle}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {children}
+          </ModalBody>
+          <ModalFooter>
+            <Button variant='ghost' colorScheme={colorScheme} mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button variant='outline' colorScheme={colorScheme}>Save Changes</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default UpdateModalContainer;
