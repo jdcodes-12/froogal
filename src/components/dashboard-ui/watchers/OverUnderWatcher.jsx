25de876ea7ac3cec8c312fb8ce6312dde3fdd015
Chris Adamson <chrisallenadamsoniii@gmail.com>
2022-10-18 21:33:00 -0500

    added array of inspirational quotes

--- src/components/dashboard-ui/watchers/OverUnderWatcher.jsx
+++ src/components/dashboard-ui/watchers/OverUnderWatcher.jsx
@@ -11,6 +11,18 @@ import { Box,
          Button,
        } from '@chakra-ui/react';
 
+
+       const quotes = [
+        "Money never made a man happy yet, nor will it. The more a man has, the more he wants. Instead of filling a vacuum, it makes one. --Benjamin Franklin",
+        "Empty pockets never held anyone back. Only empty heads and empty hearts can do that. --Norman Vincent Peale",
+        "The real measure of your wealth is how much you'd be worth if you lost all your money. --Anonymous",
+        "Wealth is not his that has it, but his that enjoys it. --Benjamin Franklin",
+        "The habit of saving is itself an education; it fosters every virtue, teaches self-denial, cultivates the sense of order, trains to forethought, and so broadens the mind. --T.T. Munger",
+        "If you want to be financially free, you need to become a different person than you are today and let go of whatever has held you back in the past. -- Robert Kiyosaki",
+        "It’s better to look ahead and prepare than to look back and regret. -- Jackie Joyner-Kersee"
+       ];
+      
+
 const OverUnderWatcher = () => {
   return (
     <Flex direction='column' justify='start'>
@@ -36,7 +48,7 @@ const OverUnderWatcher = () => {
       </Flex>
       <Flex justify='center' align='center' mt='16px'>
         <Box w='95%'>
-          <Text align='center' fontSize='lg' fontWeight='thin'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quis excepturi, suscipit modi, aliquam fuga nam a fugiat odit nostrum nulla! Fuga, quod iure praesentium a voluptas nisi ducimus nesciunt?"</Text>
+          <Text align='center' fontSize='lg' fontWeight='thin'>{quotes[Math.floor(Math.random() * quotes.length)]}</Text>
         </Box>
       </Flex>
     </Flex>
