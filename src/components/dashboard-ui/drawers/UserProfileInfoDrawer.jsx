617ab919fd50fa6a02f381c894c32a0b64abc0d3
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-19 16:01:53 -0500

    Scaffold UserProfileInfoDrawer
    Setup general layou and inputs. Need to handle state in this component. Moreover, need a way to handle drag-and-drop or upload .png or .jpg files for user avatars; consequently, we will need a place to store images files since Cloud Firestore does not store them. We can use another service from Firebase called Fire Storage which allows you to store image assets.

--- src/components/dashboard-ui/drawers/UserProfileInfoDrawer.jsx
+++ src/components/dashboard-ui/drawers/UserProfileInfoDrawer.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import  { FiUser, 
+          FiLock, 
+          FiImage,
+          FiMail
+        } from 'react-icons/fi';
+
 import  { Button,
+          Text,
+          Flex,
           Drawer,
           DrawerOverlay,
           DrawerCloseButton,
@@ -8,14 +16,23 @@ import  { Button,
           DrawerHeader,
           DrawerBody,
           DrawerFooter,
-          Input,
           useDisclosure,
-          Text,
+          FormControl,
+          FormLabel,
+          FormHelperText,
+          FormErrorMessage,
+          InputGroup,
+          InputLeftAddon,
+          Input,
+          InputRightElement
         } from '@chakra-ui/react';
 
 const UserProfileInfoDrawer = ({ linkName }) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef()
+
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
   
     // Need to setup the aria connections for better hit
     // detection in sidebar links. As of right now
@@ -36,22 +53,80 @@ const UserProfileInfoDrawer = ({ linkName }) => {
           placement='right'
           onClose={onClose}
           finalFocusRef={btnRef}
-          size = 'lg'
+          size='lg'
         >
           <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader fontSize='4xl'>User Profile Settings Go Here</DrawerHeader>
-  
+          <DrawerContent pl='16px'>
+            <DrawerCloseButton mt='16px' fontSize='16px'/>
+            <DrawerHeader fontSize='4xl' mb='16px'>My Profile</DrawerHeader>
             <DrawerBody>
-              <Input placeholder='Profile Updates...' />
+              <Flex direction='column' justify='start' gap='48px' mt='32px'>
+                <FormControl>
+                  <FormLabel fontSize='2xl' fontWeight='medium'>First Name</FormLabel>
+                  <InputGroup size='lg' variant='flushed'>
+                    <InputLeftAddon children={<FiUser size='32px' />} pr='16px'/>
+                    <Input type='text' placeholder='BigMoney'/>
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontSize='2xl' fontWeight='medium'>Last Name</FormLabel>
+                  <InputGroup size='lg' variant='flushed'>
+                    <InputLeftAddon children={<FiUser size='32px' />} pr='16px'/>
+                    <Input type='text' placeholder='Millionaire'/>
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontSize='2xl' fontWeight='medium'>Avatar</FormLabel>
+                  <InputGroup size='lg' variant='flushed'>
+                    <InputLeftAddon children={<FiImage size='32px' />} pr='16px'/>
+                    <Input placeholder='Drag and drop your .png or .jpg file here'/>
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontSize='2xl' fontWeight='medium'>Email</FormLabel>
+                  <InputGroup size='lg' variant='flushed'>
+                    <InputLeftAddon children={<FiMail size='32px' />} pr='16px'/>
+                    <Input type='email' placeholder='BigMoneyMillionaire@froogal.com'/>
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontSize='2xl' fontWeight='medium'>Password</FormLabel>
+                  <InputGroup size='lg' variant='flushed'>
+                    <InputLeftAddon children={<FiLock size='32px' />} pr='16px'/>
+                    <Input
+                      pr='4.5rem'
+                      type={show ? 'text' : 'password'}
+                      placeholder='Enter new password'
+                    />
+                    <InputRightElement width='4.5rem'>
+                      <Button mb='8px' size='md' onClick={handleClick} colorScheme='purple'>
+                        {show ? 'Hide' : 'Show'}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+              </Flex>
             </DrawerBody>
-  
             <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
+              <Flex justify='space-evenly' align='center' w='full'>
+                <Button variant='unstyled' 
+                        rounded='full' 
+                        colorScheme='purple'
+                        size='lg'
+                        fontSize='xl'
+                        onClick={onClose}
+                >
+                  Cancel
+                </Button>
+                <Button variant='solid' 
+                        rounded='full' 
+                        colorScheme='purple'
+                        size='lg'
+                        fontSize='xl'
+                >
+                  Save
               </Button>
-              <Button colorScheme='blue'>Save</Button>
+              </Flex>
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
