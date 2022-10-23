606481558000267b339fbc14ac04fe574fd8fdc7
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-23 00:52:53 -0500

    Create layout for ReceiptHub's search modal
    Created ReceiptSearchModalBody, which renders a search bar and list of user receipts inside the ButtonModalContainer

--- src/components/modals/modal-bodies/receipt-bodies/ReceiptSearchModalBody.jsx
+++ src/components/modals/modal-bodies/receipt-bodies/ReceiptSearchModalBody.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
+import ItemListTile from '../../../dashboard-ui/tiles/ItemListTile';
+
+import { FiSearch } from 'react-icons/fi';
+
 import { Flex,
          FormControl,
          Input,
+         InputGroup,
+         InputLeftElement,
        } from '@chakra-ui/react';
+
+const userReceiptCollection = [
+  { receiptDate: '10/24/22', receiptName: 'Amazon.com', receiptTotalPrice: 24.99},
+  { receiptDate: '10/25/22', receiptName: 'Walmart.com', receiptTotalPrice: 164.97},
+  { receiptDate: '10/26/22', receiptName: 'Udemy.com', receiptTotalPrice: 19.99},
+];
 
 const ReceiptSearchModalBody = () => {
   return (
-   <Flex>
-    ReceiptSearchModalBody
+   <Flex direction='column' justify='left' px='8px'>
+      <FormControl my='16px'>
+        <InputGroup>
+          <InputLeftElement children={<FiSearch size='22px' />} />
+          <Input placeholder='Search receipts' variant='filled' borderRadius='xl' fontSize='xl'/>
+        </InputGroup>
+      </FormControl>
+      <ItemListTile listType='receipt' collection={userReceiptCollection} />
    </Flex>
   );
 }
