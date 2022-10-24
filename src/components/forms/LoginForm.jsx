af0bc7de92f1e73e66b3500712f3e6b3eda04c48
DylanCommean01 <DylanCommean01@yahoo.com>
2022-10-23 20:24:04 -0500

    Added polish, new functionality, and state variables

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -22,27 +22,41 @@ import {  Box,
         
 const LoginForm = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({});
+  const [auth, setAuth] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setAuth((prev) => ({ 
+      ...prev, [e.target.name]: e.target.value
+    }));
+  };
+
+  const authSubmission = () => {
+    navigate('/dashboard', { state: auth })
+  };
+
   return (
     <CardContainer>
-       <Heading pb={8} textAlign='center'>Create A Profile</Heading>
+       <Heading pb={8} textAlign='center'>Log In To Froogal</Heading>
         <Flex p={8} direction='column'>
           <Flex pt={4} pb={12} direction='column' gap={8}>
             <Flex gap={4} direction='column' justify='fill'>
               <FormControl isRequired>
                 <FormLabel>Email:</FormLabel>
-                <Input type='text' placeholder='BigMoneyMillionaire@email.com' size='lg'/>
+                <Input name="email" type='text' placeholder='BigMoneyMillionaire@email.com' size='lg' onChange={handleChange} />
               </FormControl>
               <FormControl isRequired>
                 <FormLabel>Password</FormLabel>
-                <PasswordInput size='lg'/>
+                <PasswordInput name="password" size='lg' onChange={handleChange} placeholder="Enter Password" />
               </FormControl>
             </Flex>
           </Flex>
         </Flex>
 
         <Box p={8} align='center'>
-          <Button p={6} w='100%' rightIcon={<FaArrowRight />} colorScheme='purple' variant='outline' onClick={() => navigate('/dashboard', { state: user })}>
+          <Button p={6} w='100%' rightIcon={<FaArrowRight />} colorScheme='purple' variant='outline' onClick={authSubmission}>
             <Text fontSize={24}>Login</Text>
           </Button>
         </Box>
--- src/components/forms/SignUpForm.jsx
+++ src/components/forms/SignUpForm.jsx
@@ -24,7 +24,27 @@ import { Box,
 
 const SignUpForm = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({firstName: "", lastName: "", email: "", password: ""});
+  const [user, setUser] = useState({
+    firstName: "", 
+    lastName: "", 
+    email: "", 
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => ({
+      ...prev, [e.target.name]: e.target.value
+    }));
+  };
+
+  const userSubmission = () => { 
+      // const { message } = addUser(user);
+      const message = "";
+      if (!message) {
+        navigate('/dashboard', { state: user });
+      }
+  };
+
   return (
     <Box py={8} 
          bgColor='whiteAlpha.600'
@@ -33,32 +53,28 @@ const SignUpForm = () => {
          borderRadius='2xl'
          boxShadow='xl'
          >
-      <Heading pb={8} textAlign='center'>Create A Profile</Heading>
-      <Flex p={8} direction='column'>
-        <Flex pt={4} pb={12} direction='column' gap={8}>
+      <Heading pb={4} textAlign='center'>Create A Froogal Account</Heading>
+      <Flex p={4} direction='column'>
+        <Flex pt={4} pb={8} direction='column' gap={8}>
           <Flex gap={4}>
             <FormControl isRequired>
               <FormLabel>First Name:</FormLabel>
-              <Input type='text' placeholder='John' size='lg' onChange={(e) => setUser({...user, firstName: `${e.target.value}`})}/>
+              <Input name="firstName" type='text' placeholder='John' size='lg' onChange={handleChange} />
             </FormControl>
             <FormControl isRequired>
               <FormLabel>Last Name:</FormLabel>
-              <Input type='text' placeholder='Doe' size='lg' onChange={(e) => setUser({...user, lastName: `${e.target.value}`})}/>
+              <Input name="lastName" type='text' placeholder='Doe' size='lg' onChange={handleChange} />
             </FormControl>
           </Flex>
 
           <FormControl isRequired>
               <FormLabel>Email</FormLabel>
-              <Input type='email' placeholder='FutureMillionaire@froogal.com' size='lg' onChange={(e) => setUser({...user, email: `${e.target.value}`})}/>
+              <Input name="email" type='email' placeholder='FutureMillionaire@froogal.com' size='lg' onChange={handleChange}/>
               <FormHelperText>We'll never share your email.</FormHelperText>
           </FormControl>
         </Flex>
 
-        <Center>
-          <Divider w='50%' borderColor='gray.600'/>
-        </Center>
-
-        <Flex pt={12} pb={4} direction='column' gap={8}>
+        <Flex pb={4} direction='column' gap={8}>
           <Box px={8} py={4}>
             <Text fontStyle='normal' fontWeight='medium'>
               <Highlight query={['6 characters long', 'one capital letter', 'one symbol']}
@@ -71,7 +87,7 @@ const SignUpForm = () => {
           
           <FormControl isRequired>
             <FormLabel>Password</FormLabel>
-            <PasswordInput size='lg' onChange={(e) => setUser({...user, password: `${e.target.value}`})}/>
+            <PasswordInput name="password" size='lg' onChange={handleChange}/>
           </FormControl>
 
           <FormControl isRequired>
@@ -88,10 +104,7 @@ const SignUpForm = () => {
           rightIcon={<FaArrowRight />} 
           colorScheme='purple' 
           variant='outline' 
-          onClick={() => { 
-            // addUser(user);
-            navigate('/dashboard', { state: user });
-          }}>
+          onClick={userSubmission}>
           <Text fontSize={24}>Sign Up Now</Text>
         </Button>
       </Box>
