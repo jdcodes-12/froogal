c9e8198a6508966fd4a1fc41f01291949766fc3c
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-24 21:17:17 -0500

    Implement OverUnderSignaler dark mode scheme
    Added the dark mode palette to OverUnderSignaler component

--- src/components/dashboard-ui/watchers/OverUnderWatcher.jsx
+++ src/components/dashboard-ui/watchers/OverUnderWatcher.jsx
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from 'react';
 import { getQuotes } from '../../../utils/functions/getQuotes';
+
 import { Box,
          Flex,
          Heading,
@@ -8,7 +9,7 @@ import { Box,
          Center,
          Stat,
          StatArrow,
-         Button,
+         useColorModeValue,
        } from '@chakra-ui/react';
 
 const OverUnderWatcher = () => {
@@ -20,13 +21,20 @@ const OverUnderWatcher = () => {
   useEffect(() => {
     fetchQuotes();
   },[]);
+
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  const badgeBg = useColorModeValue('brand.lightmode.secondary.base', 'brand.darkmode.secondary.base');
+  const badgeColor = useColorModeValue('brand.lightmode.secondary.50', 'brand.darkmode.gray.700');
+  const arrowColor = useColorModeValue('brand.lightmode.success.500', 'brand.darkmode.success.300');
+
   return (
     <Flex direction='column' justify='start'>
       <Flex justify='space-between' align='center' px='8px'>
         <Heading as='h2' fontSize='2xl'>You are...</Heading>
         <Badge fontSize='xl' 
-               colorScheme='purple' 
+               bg={badgeBg}
+               color={badgeColor}
                py='2px' 
                px='16px' 
                rounded='sm'
@@ -38,7 +46,7 @@ const OverUnderWatcher = () => {
       <Flex justify='center' align='center' px='8px' py='32px'>
         <Box>
           <Stat>
-            <StatArrow boxSize={12} type='increase' size='lg'/>
+            <StatArrow boxSize={12} type='increase' size='lg' color={arrowColor} />
           </Stat>
         </Box>
         <Text ml='8px' fontSize='4xl' fontWeight='medium'>Over</Text>
