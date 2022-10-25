b1d346bd5ba948dab31463b1bf4aa480b43415e4
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-24 22:13:56 -0500

    Refactor BudgetComparer
    Remove unused imports, restructure object layouts in array, and remove white space

--- src/components/dashboard-ui/charts/BudgetComparerChart.jsx
+++ src/components/dashboard-ui/charts/BudgetComparerChart.jsx
@@ -1,63 +1,44 @@
-import { Box,
-  Flex,
-  Heading,
-  Badge,
-  Center,
-  Stat,
-  StatNumber,
-  Button,
-} from '@chakra-ui/react';
 import React from 'react';
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import  { Flex,
+          Heading,
+          Badge,
+          Center,
+          useColorModeValue,
+        } from '@chakra-ui/react';
+
+import  { LineChart, 
+          Line, 
+          XAxis, 
+          YAxis, 
+          CartesianGrid, 
+          Tooltip, 
+          Legend, 
+          ResponsiveContainer 
+        } from 'recharts';
 
 const data = [
-  {
-    month: 'Jan',
-    budget: 4000,
-    amountSpent: 2330,
-  
-  },
-  {
-    month: 'Feb',
-    budget: 3000,
-    amountSpent: 2890,
-  },
-  {
-    month: 'Mar',
-    budget: 3000,
-    amountSpent: 3530,
-  },
-  {
-    month: 'Apr',
-    budget: 3500,
-    amountSpent: 3300,
-  },
-  {
-    month: 'May',
-    budget: 4000,
-    amountSpent: 5300,
-  },
-  {
-    month: 'Jun',
-    budget: 4200,
-    amountSpent: 3000,
-  },
-  {
-    month: 'Jul',
-    budget: 5000,
-    amountSpent: 4400,
-  },
+  { month: 'Jan', budget: 4000.00, amountSpent: 2330.00, },
+  { month: 'Feb', budget: 3000.00, amountSpent: 2890.00, },
+  { month: 'Mar', budget: 3000.00, amountSpent: 3530.00, },
+  { month: 'Apr', budget: 3500.00, amountSpent: 3300.00, },
+  { month: 'May', budget: 4000.00, amountSpent: 5300.00, },
+  { month: 'Jun', budget: 4200.00, amountSpent: 3000.00, },
+  { month: 'Jul', budget: 5000.00, amountSpent: 4400.00, },
 ];
 
 const BudgetComparerChart = () => {
-  return (
 
-<Flex direction='column' justify='start'>
+  const badgeBg = useColorModeValue('brand.lightmode.secondary.base', 'brand.darkmode.secondary.base');
+  const badgeColor = useColorModeValue('brand.white.base', 'brand.darkmode.gray.700');
+
+  return (
+    <Flex direction='column' justify='start'>
       <Flex justify='space-between' align='center' px='8px'>
         <Heading as='h2' fontSize='2xl'>Budget Comparer Chart</Heading>
         <Badge fontSize='xl' 
-               colorScheme='green' 
+               color={badgeColor}
+               bg={badgeBg}
                py='2px' 
                px='16px' 
                rounded='sm'
@@ -66,31 +47,22 @@ const BudgetComparerChart = () => {
             <Center>Monthly</Center>
           </Badge>
       </Flex>
-<Flex>
-<ResponsiveContainer width='100%' height={400}>
-      <LineChart
-        
-          data={data}
-          margin={{
-            top: 30,
-            right: 50,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="budget" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="amountSpent" stroke="#82ca9d" />
-        </LineChart>
+      <Flex>
+        <ResponsiveContainer width='100%' height={400}>
+          <LineChart  data={data}
+                    margin={{ top: 30, right: 50, left: 20, bottom: 5}}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="budget" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="amountSpent" stroke="#82ca9d" />
+          </LineChart>
         </ResponsiveContainer>
-        </Flex>
       </Flex>
-    
-    
+    </Flex>
   );
 };
 
