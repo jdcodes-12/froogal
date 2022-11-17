0e372b4f2d7309c8e8390e543133534b4b4c4bca
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-11-16 18:11:14 -0600

    Scaffold search functionality for ReceiptList and ExpenseListModal
    Setup functionality for filter the list of items. However, current implementation only pulls from hardcoded values. Need to address the error when pulling from Firestore. Also, refactored ListView to be more readable & used object destructuring on the props for ReceiptListItem and ExpenseListItemLayout components

--- src/components/modals/modal-bodies/expense-bodies/ExpenseSearchModalBody.jsx
+++ src/components/modals/modal-bodies/expense-bodies/ExpenseSearchModalBody.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useContext } from 'react';
 import ItemListTile from '../../../dashboard-ui/tiles/ItemListTile';
 import { FiSearch } from 'react-icons/fi';
 import { 
@@ -9,21 +10,50 @@ import {
 } from '@chakra-ui/react';
 
 const userExpenseCollection = [
-  { expenseDueDate: '10/24/22', expenseName: 'Netflix', expensePrice: 14.99, status: 'pending'},
-  { expenseDueDate: '10/25/22', expenseName: 'TradingView', expensePrice: 14.99, status: 'unpaid'},
-  { expenseDueDate: '10/26/22', expenseName: 'LeetCode', expensePrice: 34.99, status: 'pending'},
+  { id: 1, expenseDueDate: '10/24/22', expenseName: 'Netflix', expensePrice: 14.99, status: 'pending'},
+  { id: 2, expenseDueDate: '10/25/22', expenseName: 'TradingView', expensePrice: 14.99, status: 'unpaid'},
+  { id: 3, expenseDueDate: '10/26/22', expenseName: 'LeetCode', expensePrice: 34.99, status: 'pending'},
 ];
 
 const ExpenseSearchModalBody = () => {
+  const [searchField, setSearchField] = useState('');
+  const [userExpensesList, setUserExpensesList] = useState([]);
+  const [filteredExpenses, setFilteredExpenses] = useState(userExpensesList);
+
+  console.log(userExpenseCollection);
+  useEffect(() => {
+    // const getReceiptsFromDB = async () => {
+    //   const receipts = await getReceipts(currentUser.id);
+    //   console.log(receipts);
+    //   setUserReceipts(receipts);
+    // }
+    
+    // getReceiptsFromDB();
+    setUserExpensesList(userExpenseCollection);
+  }, []);
+
+  useEffect(() => {
+    const newFilteredList = userExpensesList.filter((expense) => {
+      return expense.expenseName.toLowerCase().includes(searchField);
+    });
+
+    setFilteredExpenses(newFilteredList);
+  }, [searchField, userExpensesList]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  }
+
   return (
    <Flex direction='column' justify='left' px='8px'>
       <FormControl my='16px'>
         <InputGroup>
           <InputLeftElement children={<FiSearch size='22px' />} />
-          <Input placeholder='Search an expense to view' variant='filled' borderRadius='xl' fontSize='xl'/>
+          <Input placeholder='Search an expense to view' variant='filled' borderRadius='xl' fontSize='xl' onChange={onSearchChange} />
         </InputGroup>
       </FormControl>
-      <ItemListTile listType='expense' collection={userExpenseCollection} />
+      <ItemListTile listType='expense' collection={filteredExpenses} />
    </Flex>
   );
 }
--- src/components/modals/modal-bodies/receipt-bodies/ReceiptSearchModalBody.jsx
+++ src/components/modals/modal-bodies/receipt-bodies/ReceiptSearchModalBody.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../../../context/authContext';
+import { getReceipts } from '../../../../utils/database-functions/getReceipts';
 import ItemListTile from '../../../dashboard-ui/tiles/ItemListTile';
 import { FiSearch } from 'react-icons/fi';
 import { 
@@ -7,25 +10,57 @@ import {
   InputGroup,
   InputLeftElement,
 } from '@chakra-ui/react';
+import { getFirestore } from 'firebase/firestore';
 
 const userReceiptCollection = [
-  { receiptDate: '10/24/22', receiptName: 'Amazon.com', receiptTotalPrice: 24.99},
-  { receiptDate: '10/25/22', receiptName: 'Walmart.com', receiptTotalPrice: 164.97},
-  { receiptDate: '10/26/22', receiptName: 'Udemy.com', receiptTotalPrice: 19.99},
+  { id: 1, receiptDate: '10/24/22', receiptName: 'Amazon.com', receiptTotalPrice: 24.99},
+  { id: 2, receiptDate: '10/25/22', receiptName: 'Walmart.com', receiptTotalPrice: 164.97},
+  { id: 3, receiptDate: '10/26/22', receiptName: 'Udemy.com', receiptTotalPrice: 19.99},
 ];
 
 const ReceiptSearchModalBody = () => {
+  const [searchField, setSearchField] = useState('');
+  const [userReceiptsList, setUserReceipts] = useState([]);
+  const [filteredReceipts, setFilteredReceipts] = useState(userReceiptsList);
+
+  // const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    // const getReceiptsFromDB = async () => {
+    //   const receipts = await getReceipts(currentUser.id);
+    //   console.log(receipts);
+    //   setUserReceipts(receipts);
+    // }
+    
+    // getReceiptsFromDB();
+    setUserReceipts(userReceiptCollection);
+  }, []);
+
+  useEffect(() => {
+    const newFilteredList = userReceiptsList.filter((receipt) => {
+      return receipt.receiptName.toLowerCase().includes(searchField);
+    });
+
+    setFilteredReceipts(newFilteredList);
+  }, [searchField, userReceiptsList])
+
+  const onSearchChange = (event) => {
+      const searchFieldString = event.target.value.toLowerCase();
+      setSearchField(searchFieldString);
+  }
+
   return (
    <Flex direction='column' justify='left' px='8px'>
       <FormControl my='16px'>
         <InputGroup>
           <InputLeftElement children={<FiSearch size='22px' />} />
-          <Input placeholder='Search a receipt to view' variant='filled' borderRadius='xl' fontSize='xl'/>
+          <Input placeholder='Search a receipt to view' variant='filled' borderRadius='xl' fontSize='xl' onChange={onSearchChange} />
         </InputGroup>
       </FormControl>
-      <ItemListTile listType='receipt' collection={userReceiptCollection} />
+      <ItemListTile listType='receipt' collection={filteredReceipts} />
    </Flex>
   );
 }
 
 export default ReceiptSearchModalBody;
+
